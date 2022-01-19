36e78c516d34d75d1edcde8dbb34569819542244
Akaluka Chibueze Cyril <akalukacyril@gmail.com>
2022-01-20 00:51:51 +0200

    Created API Project

---
+++ FibonacciGeneratorAPI/Controllers/FibonacciGeneratorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FibonacciGeneratorAPI.Controllers
+{
+    public class FibonacciGeneratorController : ControllerBase
+    {
+        [HttpGet(Routes.FibonacciSubsequence)]
+        public async Task<IActionResult>GetFibonacciSubsequence()
+        {
+            return await Task.Run(Ok);
+        }
+    }
+}
---
+++ FibonacciGeneratorAPI/Controllers/Routes.cs
@@ -0,0 +1,8 @@
+﻿namespace FibonacciGeneratorAPI.Controllers
+{
+    internal static class Routes
+    {
+        private const string Base = "api";
+        public const string FibonacciSubsequence = Base + "/fibonacci-subsequence";
+    }
+}
