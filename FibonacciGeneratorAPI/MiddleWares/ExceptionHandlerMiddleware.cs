00f19bb8d6dddf0afedaa5f7d35dac411697d169
Akaluka Chibueze Cyril <akalukacyril@gmail.com>
2022-01-22 04:39:53 +0200

    Added Exception handling and logging
      - Added and configured Serilog
      - Added Exception handler middleware

---
+++ FibonacciGeneratorAPI/MiddleWares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FibonacciGeneratorAPI.MiddleWares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            _logger.LogError(ex.ToString());
+            var errorMessage = JsonSerializer.Serialize(new { Message = ex.Message, Code = "GE" });
+
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            return context.Response.WriteAsJsonAsync(errorMessage);
+        }
+    }
+}
